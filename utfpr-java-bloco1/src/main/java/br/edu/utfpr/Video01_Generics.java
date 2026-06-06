464ff2bad6c48b7b8da3f290dc79ec97ca49d3a4
Arthur Gregorio <arthurshakal@gmail.com>
2026-06-06 20:30:29 -0300

    updates

--- utfpr-java-bloco1/src/main/java/br/edu/utfpr/Video01_Generics.java
+++ utfpr-java-bloco1/src/main/java/br/edu/utfpr/Video01_Generics.java
@@ -16,7 +16,7 @@ import java.util.List;
  */
 public class Video01_Generics {
 
-    static abstract class Pagamento {
+    static class Pagamento {
 
         private final double valor;
 
@@ -43,6 +43,13 @@ public class Video01_Generics {
         }
     }
 
+    static class PixTurbo extends Pix {
+
+        public PixTurbo(double valor, String chave) {
+            super(valor, chave);
+        }
+    }
+
     static class Boleto extends Pagamento {
 
         private final String codigoBarras;
@@ -59,25 +66,25 @@ public class Video01_Generics {
 
     // Bounded type parameter, <T extends Pagamento> garante que só aceitamos pagamentos e ainda podemos chamar
     // valor() com segurança
-    static double somar(List<Pagamento> pagamentos) {
-
-        double total = 0;
-
-        for (Pagamento p : pagamentos) {
-            total += p.valor();
-        }
-        return total;
-    }
-
-//    static <T extends Pagamento> double somar(List<T> pagamentos) {
+//    static double somar(List<Pagamento> pagamentos) {
 //
 //        double total = 0;
 //
-//        for (T p : pagamentos) {
+//        for (Pagamento p : pagamentos) {
 //            total += p.valor();
 //        }
 //        return total;
 //    }
+
+    static <T extends Pagamento> double somar(List<T> pagamentos) {
+
+        double total = 0;
+
+        for (T p : pagamentos) {
+            total += p.valor();
+        }
+        return total;
+    }
 
     // Wildcards - ? extends (PECS - "Producer Extends") apenas lemos da lista, então usamos extends.
     static double somarLeitura(List<? extends Pagamento> pagamentos) {
@@ -97,7 +104,28 @@ public class Video01_Generics {
         // destino.add(new Boleto(...)); // NÃO compila, garante segurança
     }
 
+    static class Zelle {
+
+    }
+
     static void main() {
+
+//        final List pagamentos = new ArrayList(); // << java 4 | List<Object> no java 5+
+//        pagamentos.add(new Pix(100.0, "11999998888"));
+//        pagamentos.add(new Boleto(120.0, "11111111"));
+//        pagamentos.add(new Zelle());
+//
+//        final var pagamento = pagamentos.get(2);
+//
+//        if (pagamento instanceof Pix) {
+//            final var pix = (Pix) pagamento;
+//            IO.println("Chave: " + pix.chave());
+//        }
+
+//        final List<Pagamento> pagamentos = new ArrayList<>(); // type-safety
+//        pagamentos.add(new Pix(100.0, "11999998888"));
+//        pagamentos.add(new Boleto(100.0, "11999998888"));
+//        pagamentos.add(new Zelle());
 
         final List<Pix> pixes = new ArrayList<>();
         pixes.add(new Pix(100.0, "11999998888"));
@@ -106,8 +134,12 @@ public class Video01_Generics {
         final List<Boleto> boletos = new ArrayList<>();
         boletos.add(new Boleto(80.0, "34191.79001..."));
 
-//        IO.println("Total pix (bounded): " + somar(pixes));
-//        IO.println("Total boleto (bounded): " + somar(boletos));
+//        if (pixes.getClass().isAssignableFrom(boletos.getClass())) {
+//            IO.println("sim");
+//        }
+
+        IO.println("Total pix (bounded): " + somar(pixes));
+        IO.println("Total boleto (bounded): " + somar(boletos));
 
         IO.println("Total pix (extends): " + somarLeitura(pixes));
         IO.println("Total boleto (extends): " + somarLeitura(boletos));
